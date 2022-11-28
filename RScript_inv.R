82860b718ab4ef83ca1264b5bfa44402d850575e
Eila Forsman <Eila@Eilas-MacBook-Air.local>
2022-11-28 17:42:18 +0100

    Script

--- RScript_inv.R
+++ RScript_inv.R
@@ -29,7 +29,7 @@ m = lm(invdat$Höjd~invdat$Behandling)
 anova(m)
 
 summary(m)
-qqnorm(invdat$Höjd)
+hist(invdat$Höjd)
 
 m1 = lm(invdat$Höjd~invdat$Behandling-1)
 summary(m1)
@@ -38,32 +38,55 @@ m = lm(invdat$Medel.diameter~invdat$Behandling)
 anova(m)
 summary(m)
 
-qqnorm(invdat$Medel.diameter)
+hist(invdat$Medel.diameter)
 
 m1 = lm(invdat$Medel.diameter~invdat$Behandling-1)
 summary(m1)
 
-qqnorm(invdat$Medelantal)
+hist(invdat$Medelantal)
 invdat$logantal <- log(invdat$Medelantal)
-qqnorm(invdat$logantal)
+hist(invdat$logantal)
 
-m = lm(invdat$logantal~invdat$Behandling) #Ändra ordning så kontroll är intercept
+
+invdat$Behandling = factor(invdat$Behandling, levels=c("Kontroll", "Klippt", "Heatweed"))
+#Change order so Kontroll is intercept
+m = lm(invdat$logantal~invdat$Behandling) 
 anova(m)
 summary(m)
+SS_T = 9.0346+16.3737 # 25.4083
+9.0346/SS_T #0.3556 Meaning 35.56% of variance is explained 
 
-m1 = lm(invdat$logantal~invdat$Behandling-1)
+
+m1 = lm(invdat$logantal~invdat$Behandling-1) #P only shows difference from 0
 summary(m1)
 
-qqnorm(invdat$Area)
+hist(invdat$Area)
 invdat$logarea <- log(invdat$Area)
-qqnorm(invdat$logarea)
+hist(invdat$logarea)
 
-m = lm(invdat$logarea~invdat$Behandling) #Ändra ordning så kontroll är intercept
+invdat$Behandling = factor(invdat$Behandling, levels=c("Kontroll", "Klippt", "Heatweed"))
+#Change order so kontroll is intercept
+m = lm(invdat$logarea~invdat$Behandling) 
 anova(m)
-summary(m)
+summary(m) #25.2% of variance explained
 
 m1 = lm(invdat$logarea~invdat$Behandling-1)
 summary(m1)
 
-
+ggplot(invdat,(x=X), y=Medel.diameter) +
+  #geom_bar(width = 0.75, stat = "identity", position ="dodge", alpha = 0.8) +
+  #geom_errorbar(data=FDAMeans, aes(ymin= mean.antal - se.FDA, ymax=mean.antal + se.FDA),
+                #width = 0.13, alpha = 1, position=position_dodge(0.75)) +
+  theme_classic() + 
+  #scale_fill_manual(values=c("#73ba2e", "#034c77")) +
+  #scale_color_manual(values=c("#000000", "#000000")) +
+  scale_y_continuous(limits = c(0,150), expand = c(0,0)) +
+  labs(y="Medelantal per prov", x="", title = "Levande celler per tvärsnitt från varje lokal") +
+  theme(legend.position = c(0.9,0.9), 
+        legend.title = element_blank(),
+        plot.title = element_text (hjust = 0.5),
+        text = element_text(size=28, family= "Times"),
+        axis.text.x = element_text(size = 20, angle = 60,
+                                   hjust = 1, color = "grey1")) +
+  theme(axis.ticks.length=unit(.25, "cm"))
 
