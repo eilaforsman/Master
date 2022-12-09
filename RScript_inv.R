5e3a1c1e2ebe4018f8e618692e212dfa2adbd686
Eila Forsman <Eila@Eilas-MacBook-Air.local>
2022-12-09 12:11:03 +0100

    Script

--- RScript_inv.R
+++ RScript_inv.R
@@ -34,48 +34,48 @@ str(invdat_sub)
 
 #Data exploration and model fitting####
 
-invdat$Behandling = factor(invdat$Behandling, levels=c("Kontroll", "Klippt", "Heatweed"))
+invdat_sub$Behandling = factor(invdat_sub$Behandling, levels=c("Kontroll", "Klippt", "Heatweed"))
 #Change order so Kontroll is intercept
 
 #Height
-m = lm(invdat$Höjd~invdat$Behandling)
+m = lm(invdat_sub$Höjd~invdat_sub$Behandling)
 anova(m)
 
 summary(m)#48.1% of variance described
-hist(invdat$Höjd)
+hist(invdat_sub$Höjd)
 
-m1 = lm(invdat$Höjd~invdat$Behandling-1)
+m1 = lm(invdat_sub$Höjd~invdat_sub$Behandling-1)
 summary(m1)
 
 #Mean diameter
-m2 = lm(invdat$Medel.diameter~invdat$Behandling)
+m2 = lm(invdat_sub$Medel.diameter~invdat_sub$Behandling)
 anova(m2)
 summary(m2)#53.9% of variance described
 
-hist(invdat$Medel.diameter)
+hist(invdat_sub$Medel.diameter)
 
-m1 = lm(invdat$Medel.diameter~invdat$Behandling-1)
+m1 = lm(invdat_sub$Medel.diameter~invdat_sub$Behandling-1)
 summary(m1)
 
 #Mean number
 library(lme4)
 library(MASS)
 
-hist(invdat$Medelantal)
-invdat$logantal <- log(invdat$Medelantal)
-hist(invdat$logantal)
+hist(invdat_sub$Medelantal)
+invdat_sub$logantal <- log(invdat_sub$Medelantal)
+hist(invdat_sub$logantal)
 
-m3 = glm.nb(Medelantal~Behandling, data=invdat_sub) 
+m3 = lm(logantal~Behandling, data=invdat_sub) 
 anova(m3)
-summary(m3) #35.6% of variance is explained 
+summary(m3) #OBS log scale, #35.6% of variance is explained 
 
-m1 = lm(invdat$logantal~invdat$Behandling-1) #P only shows difference from 0
-summary(m1) #OBS log scale
+m1 = lm(invdat_sub$logantal~invdat_sub$Behandling-1) 
+summary(m1) 
 
 coefs = summary(m1)$coef
-exp(coefs[1,1])
-exp(coefs[2,1])
-exp(coefs[3,1])
+exp(coefs[1,1]) # [1] 17.22322 Control
+exp(coefs[2,1]) # [1] 4.543839 Mowed
+exp(coefs[3,1]) # [1] 3.05623 HW
 
 #Area
 hist(invdat_sub$Area)
