646939e9c17a05c14ee98b32768062fd3a1965c2
Eila Forsman <Eila@Eilas-MacBook-Air.local>
2023-02-27 17:11:11 +0100

    PI Script

--- RScript_PI.R
+++ RScript_PI.R
@@ -27,6 +27,8 @@ summary(dataPI)
 
 library(plyr)
 dataPI_sub <- subset(dataPI, Lokal != "H_Pålsjö_efter_HW") #Remove "efter_HW" because its a different treatment
+dataPI_sub = subset(dataPI_sub, PI != "död med microliv") 
+dataPI_sub = subset(dataPI_sub, PI != "död+död med microliv")
 str(dataPI_sub)
 head(dataPI_sub)
 
@@ -134,26 +136,27 @@ boxplot (sqrt(dataPI_sub$Antal) ~ dataPI_sub$Behandling)
 resid(m5)
 boxplot(resid(m5) ~ dataPI_sub$Behandling)
 
-m6 = glmer.nb(Antal ~ Behandling + (1|Lokal), family="poisson", data=dataPI_sub)
 m7 = glmmTMB(Antal ~ Behandling + (1|Lokal/Prov), family="nbinom1", data=dataPI_sub)
-summary(m6) 
 summary(m7)#Includes site and sample as random factor and corrects for overdispersion, 
 #correct data distribution
 
-boxplot(resid(m6) ~ dataPI_sub$Behandling)
 boxplot(resid(m7) ~ dataPI_sub$Behandling)
 
 plot(Antal ~ Behandling, data=dataPI_sub) #Visualize data distribution between treatments
 
-m10 = glmmTMB(Antal ~ Behandling, family="nbinom1", data=dataPI_sub) #Trying wothout random effects to see how they affect the result
+m10 = glmmTMB(Antal ~ Behandling, family="nbinom1", data=dataPI_sub) #Trying without random effects to see how they affect the result
 summary(m10)
 
 #Results####
+m7 = glmmTMB(Antal ~ Behandling + (1|Lokal/Prov) -1, family="nbinom1", data=dataPI_sub)
+summary(m7)
 
 coef = summary(m7)
 coef = coef[["coefficients"]][["cond"]]
-exp(coef[1,1])
-exp(coef[1,1] + coef[2,1])
+exp(coef[1,1]) #Mean control [1] 0.01350465
+exp(coef[2,1])  #Mean HW [1] 0.1331109
+exp(coef[1,2]) #SE control [1] 4.648901
+exp(coef[2,2]) #SE HW [1] 1.962618
 
 #Create data frame with statistics for each treatment
 stats = ddply(dataPI_sub, "Behandling", summarize, 
@@ -227,6 +230,12 @@ Meantot$mean_sample = Sample$V1
 Meantot$sd = Sample$V2
 Meantot$se = Sample$V3
 
+#Change names of sites for controls and gothenburg
+Meantot$newprov = gsub("Helsingborg_control","Control Helsingborg", Meantot$newprov)
+Meantot$newprov = gsub("Motala_control","Control Motala", Meantot$newprov)
+Meantot$newprov = gsub("Vellinge_control","Control Vellinge", Meantot$newprov)
+Meantot$newprov = gsub("Invaterm", "Partille", Meantot$newprov)
+
 ggplot(Meantot, aes(x=reorder(newprov, order), y=mean_sample)) +
   geom_bar(width = 0.75, stat = "identity", position ="dodge", alpha = 0.8) +
   geom_errorbar(data=Meantot, aes(ymin= mean_sample - se, 
@@ -234,7 +243,7 @@ ggplot(Meantot, aes(x=reorder(newprov, order), y=mean_sample)) +
                 width = 0.13, alpha = 1, position=position_dodge(0.75)) +
   theme_classic() + 
   scale_y_continuous(limits = c(0,20), expand = c(0,0)) +
-  labs(y="Mean number of cells per sample", x="", 
+  labs(y="Mean number of cells", x="", 
        title = "") +
   theme(legend.position = c(0.9,0.9), 
         legend.title = element_blank(),
@@ -303,9 +312,8 @@ r2(m13)
 
 plot(dataPI_sub$Antal ~ dataPI_sub$Behandling, las=1)
 
-ggplot(dataPI_sub, aes(x=Behandling, y=Antal, fill=Behandling)) +
+ggplot(dataPI_sub, aes(x=Behandling, y=Antal)) +
   geom_boxplot() +
-  scale_fill_grey() +
   theme_classic() + 
   scale_y_continuous(limits = c(0,160), expand = c(0,0)) +
   labs(y="Number of dead cells", x="", 
@@ -326,42 +334,32 @@ ggsave("Behandling_PI_boxplot.png", plot = last_plot(), device = "png",
 
 plot(datHW$Antal ~ datHW$Kommun, las=1) #visualize data
 
-mm = glm.nb(Antal ~ Kommun, data=datHW) #Fit model for number of cells per micrograph
-anova(mm)
-summary(mm) #Bad fit
+kommunmeans <- ddply(meantot_sub,"Kommun", summarize, N=length(mean_sample),
+                     mean = mean(na.omit(mean_sample)),
+                     sd.PI = sd(na.omit(mean_sample)),
+                     se.PI = sd.PI/sqrt(N))
 
-mm1 = glm.nb(Antal ~ Kommun-1, data=datHW)
-summary(mm1) #Bad fit
-
-coef2 = summary(mm1)$coef
-exp(coef2[1,1]) #24.69804 mean Gothenburg
-exp(coef2[2,1]) #21.44664 mean Helsingborg
-exp(coef2[3,1]) #23.43799 mean Motala
-exp(coef2[4,1]) #4.520792 mean Vellinge
-
-datHW$Kommun = factor(datHW$Kommun, levels=c("Vellinge", "Göteborg", "Helsingborg", "Motala"))
+datHW$Kommun = factor(datHW$Kommun, levels=c("Vellinge", "Motala", "Göteborg", "Helsingborg"))
 
 mm = glmmTMB(Antal ~ Kommun + (1|Lokal/Prov), family="nbinom1", datHW)
 summary(mm) #Fit model where site and sample are random factors
-#No sig dif.
 
-mm2 = glmmTMB(Antal ~ Kommun + (1|Lokal/Prov), family="nbinom1", datC)
-summary(mm2) #Model difference between municipalities in control site, NA produced
+mm2 = glmmTMB(Antal ~ Kommun + (1|Lokal/Prov) -1, family="nbinom1", datHW)
+summary(mm2)
 
 boxplot(resid(mm) ~ datHW$Kommun)
 
-coef4 = summary(mm)
-coef4 = coef4[["coefficients"]][["cond"]] # Mean cells per micrograph, log scale
-exp(coef4[1,1]) # [1] 0.09597268 mean V
-exp(coef4[1,1] + coef4[2,1]) # [1] 0.7730085 mean G
-exp(coef4[1,1] + coef4[3,1]) # [1] 0.40313 mean H
-exp(coef4[1,1] + coef4[4,1]) # [1] 0.05495027 mean M
+coef4 = summary(mm2)
+coef4 = coef4[["coefficients"]][["cond"]] # Mean cells per micrograph,
+exp(coef4[1,1]) # [1] 0.06782672 mean V
+exp(coef4[2,1])  # [1] 0.5272543 mean G
+exp(coef4[3,1]) # [1] 0.224372 mean H
+exp(coef4[4,1]) # [1] 0.03070283 mean M
 
-exp(coef4[1,2]) # [1] 2.088992 SE V
-exp(coef4[1,2] + coef4[2,2]) # [1] 6.328839 SE G
-exp(coef4[1,2] + coef4[3,2]) # [1] 5.350443 SE H
-exp(coef4[1,2] + coef4[4,2]) # [1] 4.777567 SE M
-
+exp(coef4[1,2]) # [1] 2.253119 SE V
+exp(coef4[2,2]) # [1] 2.606862 SE G
+exp(coef4[3,2]) # [1] 2.107843 SE H
+exp(coef4[4,2]) # [1] 1.86862 SE M
 
 #Sample modelling
 Meantot$Kommun = c("Helsingborg", "Helsingborg", "Helsingborg", "Göteborg",
@@ -371,7 +369,7 @@ meantot_sub = subset(Meantot, Meantot$newprov != "Helsingborg_control" & Meantot
 meantot_sub$Kommun = as.factor(meantot_sub$Kommun)
 str(meantot_sub)
 
-meantot_sub$Kommun = factor(meantot_sub$Kommun, levels=c("Vellinge", "Göteborg", "Helsingborg", "Motala"))
+meantot_sub$Kommun = factor(meantot_sub$Kommun, levels=c("Vellinge", "Motala", "Gothenburg", "Helsingborg"))
 
 
 hist(meantot_sub$mean_sample)
@@ -385,23 +383,24 @@ meantot_sub = na.omit(meantot_sub)
 mm2 = lm(logsample ~ Kommun, data=meantot_sub) #Fit model to log data 
 summary(mm2) 
 
-mm2 = lm(logsample ~ Kommun-1, data=meantot_sub) #Get estimated means (log scale)
+mm2 = glm(logsample ~ Kommun-1, data=meantot_sub) #Get estimated means (log scale)
 summary(mm2)
 
 coef3 = summary(mm2)$coef
-exp(coef3[1,1]) #[1] 4.32938 Mean Vellinge
-exp(coef3[2,1]) #[1] 122.446 Mean Göteborg
-exp(coef3[3,1]) # [1] 62.73146 Mean Helsingborg
-exp(coef3[4,1]) # [1] 90.49646 Mean Motala
+exp(coef3[1,1]) #[1] 0.8017943 Mean Vellinge
+exp(coef3[2,1]) #[1] 5.061254 Mean Göteborg
+exp(coef3[3,1]) # [1] 5.335916 Mean Helsingborg
+exp(coef3[4,1]) # [1] 0.8819892 Mean Motala
 
-exp(coef3[1,2]) # [1] 2.960874 SE V
-exp(coef3[2,2]) # [1] 4.641823 SE G
-exp(coef3[3,2]) # [1] 2.960874 SE H
-exp(coef3[4,2]) # [1] 1.871434 SE M
+exp(coef3[1,2]) # [1] 2.852007 SE V
+exp(coef3[2,2]) # [1] 4.402306 SE G
+exp(coef3[3,2]) # [1] 2.852007 SE H
+exp(coef3[4,2]) # [1] 1.940272 SE M
 
 meantot_sub = subset(Meantot, Meantot$newprov != "Helsingborg_control" & Meantot$newprov != "Motala_control" & Meantot$newprov != "Vellinge_control")
 meantot_sub$Kommun = as.factor(meantot_sub$Kommun)
-kruskal.test(mean_sample ~ Kommun, data=meantot_sub)
+kruskal.test(mean_sample ~ Kommun, data=meantot_sub) 
+#Kruskal-Wallis chi-squared = 2.2145, df = 3, p-value = 0.5291
 
 kommunmeans <- ddply(meantot_sub,"Kommun", summarize, N=length(mean_sample),
                      mean = mean(na.omit(mean_sample)),
@@ -412,11 +411,11 @@ kommunmeans <- ddply(meantot_sub,"Kommun", summarize, N=length(mean_sample),
 
 plot(meantot_sub$mean_sample ~ meantot_sub$Kommun)
 
-ggplot(meantot_sub, aes(x=Kommun, y=mean_sample, fill=Kommun)) +
+ggplot(meantot_sub, aes(x=Kommun, y=mean_tot, fill=Kommun)) +
   geom_boxplot() +
   scale_fill_grey() +
   theme_classic() + 
-  scale_y_continuous(limits = c(0,20), expand = c(0,0)) +
+  scale_y_continuous(limits = c(0,5), expand = c(0,0)) +
   labs(y="Number of cells", x="", 
        title = "") +
   theme(legend.position = c(0.9,0.9), 
@@ -469,36 +468,29 @@ str(HW_str)
 summary(HW_str)
 
 hist(HW_str$Antal)
+plot(HW_str$Antal ~ HW_str$Struktur)
+HW_str$Struktur = factor(HW_str$Struktur, levels=c("vascular tissue", "cortex", "pith"))
 
 mod1 = glm.nb(Antal ~ Struktur, data=HW_str)
 anova(mod1)
 summary(mod1) # sig dif betweeen structures, vt fewest cells, pith most, not great model fit
 
-coef5 = summary(mod1)$coef
-
-exp(coef5[1,1]) # [1] 28.75203 mean cortex
-exp(coef5[2,1] + coef5[1,1]) # [1] 43.55789 mean pith
-exp(coef5[3,1] + coef5[1,1]) # [1] 0.2593235 mean vt
-
-exp(coef5[1,2]) # [1] 1.086046 SE cortex
-exp(coef5[2,2] + coef5[1,2]) # [1] 1.287328 SE pith
-exp(coef5[3,2] + coef5[1,2]) # [1] 1.239278 SE vt
-
 mod2 = glmmTMB(Antal ~ Struktur + (1|Lokal/Prov), family="nbinom1", data=HW_str)
 summary(mod2) #sig dif between str, takes site and sample into account
+#Suppress intercept and calculate means and SE
+mod2 = glmmTMB(Antal ~ Struktur + (1|Lokal/Prov)-1, family="nbinom1", data=HW_str)
+summary(mod2)
 
 coef6 = summary(mod2)$coef
 coef6 = coef6[["cond"]]
 
-exp(coef6[1,1]) # [1] 0.1436847 mean cortex
-exp(coef6[1,1] + coef6[2,1]) # [1] 0.2765676 mean pith
-exp(coef6[1,1] + coef6[3,1]) # [1] 0.02647525 mean vt
+exp(coef6[1,1]) # [1] 0.01725078 mean vt
+exp(coef6[2,1]) # [1] 0.07365696 mean cortex
+exp(coef6[3,1]) # [1] 0.1834615 mean pith
 
-exp(coef6[1,2]) #[1] 1.723259 SE cortex
-exp(coef6[1,2] + coef6[2,2]) # [1] 2.052546 SE pith
-exp(coef6[1,2] + coef6[3,2]) #[1] 2.199552 SE vt
-
-#Means don't make sense should be 20 to 80 something per micrograph
+exp(coef6[1,2]) # [1] 1.791595 SE vt
+exp(coef6[2,2]) # [1] 1.743369 SE cortex
+exp(coef6[3,2]) # [1] 1.73792 SE pith
 
 kruskal.test(Antal ~ Struktur, data=HW_str)
 #Kruskal-Wallis chi-squared = 148.81, df = 2, p-value < 2.2e-16
@@ -513,11 +505,10 @@ str_means = ddply(HW_str, "Struktur", summarize,
 
 plot(Antal ~ Struktur, data=HW_str)
 
-ggplot(HW_str, aes(x=Struktur, y=Antal, fill=Struktur)) +
+ggplot(HW_str, aes(x=Struktur, y=Antal)) +
   geom_boxplot() +
-  scale_fill_grey() +
   theme_classic() + 
-  scale_y_continuous(limits = c(0,160), expand = c(0,0)) +
+  scale_y_continuous(limits = c(0,120), expand = c(0,0)) +
   labs(y="Number of cells", x="", 
        title = "") +
   theme(legend.position = c(0.9,0.9), 
@@ -556,46 +547,41 @@ se = tapply(data$Antal,
 se = na.omit(se)
 
 colMeans(means)
-#Control  Heatweed 
-#0.2232044 1.0717430  
+#  Control  Heatweed 
+#0.1983255 0.9196957 
 
 rowMeans(means)
-#  cortex            pith vascular tissue 
-#0.57902499      1.29447711      0.06891892 
+#   cortex            pith vascular tissue 
+#0.34090977      1.27015452      0.06596752 
 
 colMeans(se)
-# Control  Heatweed 
-# 0.1024732 0.2340615
+# Control   Heatweed 
+#0.09438675 0.18961361 
 
 rowMeans(se)
-#   cortex          pith   vascular tissue 
-#0.16939395      0.31242267      0.02298541 
+#     cortex            pith vascular tissue 
+#0.08903042      0.31405983      0.02291028 
 
 #mann whitney U compare seperatly
 
 d = subset(data, data$Struktur =="cortex")
 mu = wilcox.test(d$Antal ~ d$Behandling)
-mu #W = 134674, p-value = 0.1176
+#W = 132873, p-value = 0.3851
 
 d = subset(data, data$Struktur =="pith")
 mu = wilcox.test(d$Antal ~ d$Behandling)
-mu # W = 25980, p-value = 1.148e-05
+mu # W = 25970, p-value = 2.409e-05
 
 d = subset(data, data$Struktur =="vascular tissue")
 mu = wilcox.test(d$Antal ~ d$Behandling)
-mu # W = 218978, p-value = 0.02322
+mu # W = 218978, p-value = 0.02945
 
 mu_treat = wilcox.test(data$Antal ~ data$Behandling)
-mu_treat # W = 1022082, p-value = 0.0002905
-summary(mu_treat)
+mu_treat # W = 1016502, p-value = 0.001337
 
-
-#Plotting
+#Plotting str and treat
 
 library(dplyr)
-#data %>% 
-#group_by(Struktur, Behandling) %>% 
-#summarise(data_groups = mean(Antal)) -> data2
 
 data %>% 
   ggplot() +
@@ -615,6 +601,115 @@ ggsave("str_treat_interaction.png", plot = last_plot(), device = "png",
        scale = 1, width = 10, height = 8,
        dpi = 600)
 
+#Comparing control and HW separately for each municipality#######
+
+#Helsingborg
+dat_munH = subset(dataPI_sub, Kommun == "Helsingborg")
+
+hist(dat_munH$Antal, xlab="Number of cells", las=1, main=NULL)
+
+dat_munH$Behandling = factor(dat_munH$Behandling, levels=c("Control", "Heatweed"))
+
+mu_munH = wilcox.test(dat_munH$Antal ~ dat_munH$Behandling)
+mu_munH #W = 55722, p-value = 2.875e-08
+
+#Take site and sample into account
+glmmH = glmmTMB(Antal ~ Behandling + (1|Lokal/Prov), family="nbinom1", data=dat_munH)
+summary(glmmH) #No difference p = 0.997
+glmmH = glmmTMB(Antal ~ Behandling + (1|Lokal/Prov)-1, family="nbinom1", data=dat_munH)
+
+plot(Antal ~ Behandling, data = dat_munH)
+
+ggplot(dat_munH, aes(x=Behandling, y=Antal)) +
+  geom_boxplot() +
+  theme_classic() + 
+  scale_y_continuous(limits = c(0,120), expand = c(0,0)) +
+  labs(y="Number of cells", x="", 
+       title = "Helsingborg") +
+  theme(legend.position = c(0.9,0.9), 
+        legend.title = element_blank(),
+        plot.title = element_text (hjust = 0.5),
+        text = element_text(size=28, family= "Times"),
+        axis.text.x = element_text(size = 28, angle = 0,
+                                   hjust = 0.5, color = "black")) +
+  theme(axis.ticks.length=unit(.25, "cm"))
+
+ggsave("Helsingborg_PI.png", plot = last_plot(), device = "png",
+       scale = 1, width = 13, height = 8,
+       dpi = 600)
+
+#Motala
+dat_munM = subset(dataPI_sub, Kommun == "Motala")
+
+hist(dat_munM$Antal, xlab="Number of cells", las=1, main=NULL)
+
+dat_munM$Behandling = factor(dat_munM$Behandling, levels=c("Control", "Heatweed"))
+
+mu_munM = wilcox.test(dat_munM$Antal ~ dat_munM$Behandling)
+mu_munM #W = 200739, p-value = 0.0002834
+
+#Take site and sample into account
+glmmM = glmmTMB(Antal ~ Behandling + (1|Lokal/Prov), family="nbinom1", data=dat_munM)
+summary(glmmM) #No difference but p = 0.0539
+glmmM = glmmTMB(Antal ~ Behandling + (1|Lokal/Prov)-1, family="nbinom1", data=dat_munM)
+
+plot(Antal ~ Behandling, data = dat_munM)
+
+ggplot(dat_munM, aes(x=Behandling, y=Antal)) +
+  geom_boxplot() +
+  theme_classic() + 
+  scale_y_continuous(limits = c(0,60), expand = c(0,0)) +
+  labs(y="Number of cells", x="", 
+       title = "Motala") +
+  theme(legend.position = c(0.9,0.9), 
+        legend.title = element_blank(),
+        plot.title = element_text (hjust = 0.5),
+        text = element_text(size=28, family= "Times"),
+        axis.text.x = element_text(size = 28, angle = 0,
+                                   hjust = 0.5, color = "black")) +
+  theme(axis.ticks.length=unit(.25, "cm"))
+
+ggsave("Motala_PI.png", plot = last_plot(), device = "png",
+       scale = 1, width = 13, height = 8,
+       dpi = 600)
+
+#Vellinge
+dat_munV = subset(dataPI_sub, Kommun == "Vellinge")
+
+hist(dat_munV$Antal, xlab="Number of cells", las=1, main=NULL)
+
+dat_munV$Behandling = factor(dat_munV$Behandling, levels=c("Control", "Heatweed"))
+
+mu_munV = wilcox.test(dat_munV$Antal ~ dat_munV$Behandling)
+mu_munV #W = 63597, p-value = 0.007119
+
+#Take site and sample into account
+glmmV = glmmTMB(Antal ~ Behandling + (1|Lokal/Prov), family="nbinom1", data=dat_munV)
+summary(glmmV) #No difference p = 1
+glmmV = glmmTMB(Antal ~ Behandling + (1|Lokal/Prov)-1, family="nbinom1", data=dat_munV)
+
+plot(Antal ~ Behandling, data = dat_munV)
+
+ggplot(dat_munV, aes(x=Behandling, y=Antal)) +
+  geom_boxplot() +
+  theme_classic() + 
+  scale_y_continuous(limits = c(0,60), expand = c(0,0)) +
+  labs(y="Number of cells", x="", 
+       title = "Vellinge") +
+  theme(legend.position = c(0.9,0.9), 
+        legend.title = element_blank(),
+        plot.title = element_text (hjust = 0.5),
+        text = element_text(size=28, family= "Times"),
+        axis.text.x = element_text(size = 28, angle = 0,
+                                   hjust = 0.5, color = "black")) +
+  theme(axis.ticks.length=unit(.25, "cm"))
+
+ggsave("Vellinge_PI.png", plot = last_plot(), device = "png",
+       scale = 1, width = 13, height = 8,
+       dpi = 600)
 
 
+#Trying Kruskal Wallis for municipalities
 
+KW = kruskal.test(Antal ~ Kommun, data=datHW)
+KW #Kruskal-Wallis chi-squared = 85.725, df = 3, p-value < 2.2e-16
